bce3a423ef896bdef07ddf3049c3a9ecb221a6e8
Hyson <yepeanut@foxmail.com>
2026-06-07 19:13:41 +0800

    Add BaseAgent class to define common interfaces for agent implementations

---
+++ agents/base_agent.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+
+class BaseAgent(ABC):
+    '''
+    Base agent class that defines basic interfaces and common methods.
+
+    All specific agent implementations should inherit from this class and implement its abstract methods.
+
+    The primary goal is to ensure that the training workflow in main.py remains compatible with different agent implementations.
+    '''
+    def __init__(self, gamma):
+        self._gamma = gamma
+        self.t = 0
+
+    def next_step(self):
+        self.t += 1
+
+    # Abstract methods: concrete subclasses must implement these
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def update_delay(self, observation = None, ref_delay = -1):
+        pass
+
+    @abstractmethod
+    def build_state(self, observation = None):
+        pass
+
+    @abstractmethod
+    def store_transition(self, state, action, reward, next_state, prob, observation):
+        pass
+
+    @abstractmethod
+    def choose_action(self, states):
+        pass
+
+    @abstractmethod
+    def save_checkpoint(self, path):
+        pass
+
+    @abstractmethod
+    def learn(self):
+        pass
